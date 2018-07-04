ca7050e26c3959edcd43ec58cc0e8128cd86424a
julyxmutti <40809007+julyxmutti@users.noreply.github.com>
2018-07-03 22:07:43 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,9 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
+{var edad;
+    edad = prompt("Ingrese su edad:");
+    alert(edad);
 	
 }
 
